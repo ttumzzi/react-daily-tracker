239a4f823572551657ab35b5164de8e546a86f74
ttumzzi <ssumini0829@gmail.com>
2020-02-04 11:58:38 +0900

    complete checking function

--- src/component/Tracker.js
+++ src/component/Tracker.js
@@ -1,6 +1,21 @@
 import React, { Component } from "react";
 import "./Tracker.css";
+import moment from "moment";
 import Calendar from "./Calendar";
+
+function CheckButton(props) {
+  const { title, checkDates, handleCheck } = props;
+  const today = moment().date();
+  if (!checkDates.includes(today)) {
+    return (
+      <button className="item-check-button" onClick={() => handleCheck(title)}>
+        <span>✔️</span>
+      </button>
+    );
+  } else {
+    return null;
+  }
+}
 
 class Tracker extends Component {
   render() {
@@ -9,14 +24,11 @@ class Tracker extends Component {
       <div className="tracker-box">
         <div className="item-header">
           <h2>{title}</h2>
-          {!checkToday && (
-            <button
-              className="item-check-button"
-              onClick={() => handleCheck(title)}
-            >
-              <span>✔️</span>
-            </button>
-          )}
+          <CheckButton
+            title={title}
+            checkDates={checkDates}
+            handleCheck={handleCheck}
+          />
         </div>
         <div className="item-calendar">
           <Calendar checkDates={checkDates} />
