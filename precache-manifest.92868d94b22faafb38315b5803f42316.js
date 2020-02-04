41d1a10ab5c58a899c419d2e53829d71ddca3ed2
ttumzzi <ssumini0829@gmail.com>
2020-02-04 20:21:04 +0900

    Updates

---
+++ precache-manifest.92868d94b22faafb38315b5803f42316.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a74565a2287cabb6968871a76931789",
+    "url": "/daily_tracker/index.html"
+  },
+  {
+    "revision": "7f5b734272c2f6088270",
+    "url": "/daily_tracker/static/css/main.b94c3e39.chunk.css"
+  },
+  {
+    "revision": "4c74ff74eadc765cc115",
+    "url": "/daily_tracker/static/js/2.252b3bde.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/daily_tracker/static/js/2.252b3bde.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7f5b734272c2f6088270",
+    "url": "/daily_tracker/static/js/main.71026a5b.chunk.js"
+  },
+  {
+    "revision": "6de80a3d96c6d93f8187",
+    "url": "/daily_tracker/static/js/runtime-main.2212286d.js"
+  }
+]);
